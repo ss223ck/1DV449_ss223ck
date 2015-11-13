471d1b65b14b91b92d4fda594f50fa027039aad3
ss223ck <ss223ck@student.lnu.se>
2015-11-13 15:30:39 +0100

    commit

--- Lab_1/model/HandleInformation.php
+++ Lab_1/model/HandleInformation.php
@@ -5,13 +5,51 @@ namespace model;
 class HandleInformation{
     
     public function startGatherInfo($inputUrl){
-        $linkData;
         $linkData = $this->gatherInformation($inputUrl);
         
-        $linkArray = $this->gatherElementData($linkData, '//a/@href');
-        var_dump($linkArray);
+        $linkArray = $this->gatherElementData($linkData, "//a/@href");
+        
+        $possibleDates = $this->getPossibleDates($inputUrl, $linkArray[0]);
+                
+        
     }
     
+    private function getPossibleDates($startUrl, $calenderUrl){
+        //get the new link to calendar
+        $calendarInfo = $this->gatherExtensionUrl($startUrl, $calenderUrl);
+        $newLink = $this->gatherElementData($calendarInfo, "//a/@href");
+        
+        //array of individual urls
+        $linkArrayOfIndividuals = $this->gatherExtensionUrl($startUrl, $newLink[0]);
+        $linkArrayOfIndividuals = $this->gatherElementData($linkArrayOfIndividuals, "//a/@href");
+        
+        //return the dates 
+        $possibleDates = $this->checkPossibleDates($linkArrayOfIndividuals, $startUrl . $newLink[0]);
+        return $possibleDates;
+    }
+    
+    private function checkPossibleDates($linkArray, $calendarUrl){
+        $datesFromCalendar = array();
+        $possibleDates = array(0=>"ok", 1=>"ok", 2=>"ok");
+        
+        foreach($linkArray as $link) {
+            $individualCalendarData = $this->gatherExtensionUrl($calendarUrl, $link);
+            $datesFromCalendar[] = $this->gatherElementData($individualCalendarData, "//tr/td");
+        }
+        
+        //Loops throught dates to unset value "ok" if the date isnt ok.
+        foreach ($datesFromCalendar as $dates) {
+            for ($i = 0; $i < count($dates); $i++) {
+                if($possibleDates[$i] == "ok" && strcasecmp($dates[$i], "ok") != 0) {
+                    $possibleDates[$i] = "";
+                }
+            }
+            
+        }
+        
+        
+        return $possibleDates;
+    }
     
     private function gatherInformation($url) {
         $ch = curl_init();
@@ -26,8 +64,11 @@ class HandleInformation{
             curl_close($ch);
             throw new Exception("not implemented");
         }
-        
         return $data;
+    }
+    
+    private function gatherExtensionUrl($url, $extension){
+        return $this->gatherInformation($url . $extension);
     }
     
     private function gatherElementData($data, $Element){
@@ -43,4 +84,6 @@ class HandleInformation{
         }
         return $dataArray;
     }
+    
+    
 }
