af8e6c1f703dc5f92fccd40640c8df3cfc554817
ss223ck <ss223ck@student.lnu.se>
2015-12-15 20:09:34 +0100

    commi

--- Laboration_3/scripts/GoogleMaps.js
+++ Laboration_3/scripts/GoogleMaps.js
@@ -1,28 +1,48 @@
+'use strict'
+
 
 var map;
+
 function initMap() {
     
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 59.329333, lng: 18.068633},
       zoom: 8
     });
-  
-    
-var infowindow = new google.maps.InfoWindow({
-                      content: ""
-                      });
-  
-    
+            
     var xmlhttp = new XMLHttpRequest();
     var url = "http://api.sr.se/api/v2/traffic/messages?format=json&size=100";
     var trafficInformation;
     
-    
-    
-    xmlhttp.onreadystatechange = function() {
+    var a = Math.abs(new Date() - new Date(localStorage["timeOfLastCall"]));
+    a = a/1000;
+    a = a/60;
+    if(localStorage["timeOfLastCall"] == null || a > 5){
+        xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            localStorage["messages"] = xmlhttp.responseText;
-            trafficInformation = JSON.parse(localStorage["messages"]);
+                localStorage["messages"] = xmlhttp.responseText;
+            }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+        localStorage["timeOfLastCall"] = new Date();
+    }
+    if(localStorage["messages"] != null)
+    {
+        renderMarkers();
+    }
+    else
+    {
+        alert("sidan kunde inte ladda in resurser, testa att ladda om sidan!")
+    }
+    
+};
+
+function renderMarkers(){
+        var infowindow = new google.maps.InfoWindow({
+                      content: ""
+                      });;
+        var trafficInformation = JSON.parse(localStorage["messages"]);
             trafficInformation["messages"].sort(function(a, b){
                 if (a.createddate > b.createddate)
                     return -1;
@@ -30,7 +50,10 @@ var infowindow = new google.maps.InfoWindow({
                     return 1;
                 return 0; 
             });
+            
+            var i = 0;
             trafficInformation["messages"].forEach(function(obj){
+                i++;
                 var date = new Date(parseInt(obj.createddate.replace('/Date(', '')));
                 var localtime = date.toDateString('sv-SE');
                 var category;
@@ -40,52 +63,93 @@ var infowindow = new google.maps.InfoWindow({
                     map: map,
                     title: obj.title
                     });
+                var trafficM = document.getElementById("trafficMessages");
+                var message = document.createElement("div");
+                var text = document.createElement("p");
                 
-                
+                message.setAttribute("id", "messageDiv" + i);
                 switch(obj.category) {
                     case 0:
                         category = "Vägtrafik"
                         document.getElementById("roadTraffic").addEventListener('click', function(){
-                            if(map.getBounds().contains(marker.getPosition()) != false)
+                            if(marker.visible)
                             {
-                                marker.setMap(null);
+                                message.className = "hidden";
+                                marker.setVisible(false);
+                                
                             }
                             else
                             {
-                                marker = new google.maps.Marker({
-                                    position: {lat: obj.latitude , lng: obj.longitude},
-                                    map: map,
-                                    title: obj.title
-                                    });
+                                message.className = "trafficMessageDiv";
+                                marker.setVisible(true);
                             }
                         });
+                        break;
                     case 1:
                         category = "Kollektivtrafik"
                         document.getElementById("collectivTraffic").addEventListener('click', function(){
-                            marker.setMap(null);
+                            if(marker.visible)
+                            {
+                                message.className = "hidden";
+                                marker.setVisible(false);
+                                
+                            }
+                            else
+                            {
+                                message.className = "trafficMessageDiv";
+                                marker.setVisible(true);
+                            }
                         });
+                        break;
                     case 2:
                         category = "Planerad störning"
                         document.getElementById("PlanedDisturbance").addEventListener('click', function(){
-                            marker.setMap(null);
+                            if(marker.visible)
+                            {
+                                message.className = "hidden";
+                                marker.setVisible(false);
+                                
+                            }
+                            else
+                            {
+                                message.className = "trafficMessageDiv";
+                                marker.setVisible(true);
+                            }
                         });
+                        break;
                     case 3:
                         category = "Övrigt"
                         document.getElementById("other").addEventListener('click', function(){
-                            marker.setMap(null);
+                            if(marker.visible)
+                            {
+                                message.className = "hidden";
+                                marker.setVisible(false);
+                                
+                            }
+                            else
+                            {
+                                message.className = "trafficMessageDiv";
+                                marker.setVisible(true);
+                            }
                         });
+                        break;
                 }
                 switch(obj.priority) {
                     case 1:
-                        priority = "Mycket allvarlig händelse"
+                        priority = "Mycket allvarlig händelse";
+                        break;
                     case 2:
-                        priority = "Stor händelse"
+                        priority = "Stor händelse";
+                        break;
                     case 3:
-                        priority = "Störning"
+                        priority = "Störning";
+                        break;
                     case 4:
-                        priority = "Information"
+                        priority = "Information";
+                        break;
                     case 5:
-                        priority = "Mindre störning"
+                        priority = "Mindre störning";
+                        break;
                 }
                 var contentString = '<div id="content">'+
                     '<div id="siteNotice">'+
@@ -113,9 +177,7 @@ var infowindow = new google.maps.InfoWindow({
                     infowindow.open(map, marker);
                     });
                     
-                var trafficM = document.getElementById("trafficMessages");
-                var message = document.createElement("div");
-                var text = document.createElement("p");
+                
                 
                 message.className = "trafficMessageDiv";
                 text.innerHTML = obj.title + " " + localtime;
@@ -131,13 +193,9 @@ var infowindow = new google.maps.InfoWindow({
                     infowindow.open(map, marker);
                 });
             });
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();    
-};
+    }
 
-function openInfoWindow(){
+function openInfoWindow(contentString, marker){
                     infowindow.close();
                     infowindow = new google.maps.InfoWindow({
                       content: ""
