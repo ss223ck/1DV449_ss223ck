07faf351baa49a42cb2f42569d33c8e82aadc0f9
ss223ck <ss223ck@student.lnu.se>
2015-12-15 21:02:24 +0100

    Klar
    tagit bort onödiga filer

--- Laboration_3/Index.php
+++ Laboration_3/Index.php
@@ -1,19 +1,39 @@
 <?php
 
 
-require_once("controller/TrafficController.php");
-require_once("view/ComposeOutput.php");
-require_once("view/RenderPage.php");
-require_once("model/HandleInformation.php");
-require_once("model/TrafficMessage.php");
+echo '<!DOCTYPE html>
+                <html>
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="style/stylesheet.css">
+                    
+                    
+                  </head>
+                  <body>
+                    <div id="trafficContainer">
+                    <div>
+                            <label for="roadTraffic">Vägtraffik</label>
+                            <input id="roadTraffic" name="roadTraffic" type="checkbox" checked></input>
+                            <label for="collectivTraffic">kollektivtraffik</label>
+                            <input id="collectivTraffic" name="collectivTraffic" type="checkbox" checked></input>
+                            <label for="PlanedDisturbance">Planerad störningar</label>
+                            <input id="PlanedDisturbance" name="PlanedDisturbance" type="checkbox" checked></input>
+                            <label for="other">Annat</label>
+                            <input id="other" name="other" type="checkbox" checked></input>
+                        </div>
+                    <div id="trafficMessages">
+                        
+                    </div>
+                    </div>
+                    <div id="map"></div>
+                    
 
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
-
-$composeOutput = new \view\ComposeOutput();
-$renderPage = new \view\RenderPage();
-$handleInformation = new \model\HandleInformation();
-
-$controller = new \controller\TrafficController($renderPage, $composeOutput, $handleInformation);
-
-$controller->startTrafficApp();
+                    <script src="scripts/GoogleMaps.min.js" ></script>
+                    
+                    <script async defer
+                      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAez0UBVTT-gu5oNjpUmDK42WxmwOpV7xk&callback=initMap">
+                    </script>
+                                        
+                  </body>
+                </html>
+                                ';
